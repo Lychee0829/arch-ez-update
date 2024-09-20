c5930bf522b8e0fe6f86a8ebb5f9cc297181cdd4
Lychee0829 <qweasdzxc1230829@163.com>
2024-09-20 19:37:29 +0800

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
+
+# 显示系统信息
 neofetch
-cat .sysupdatelog | while read line
-do
-    echo $line
+
+# 读取并显示 .sysupdatelog 文件内容
+cat .sysupdatelog | while read line; do
+    echo "$line"
 done
-#读取上次更新时间
+
+# 读取上次更新时间
 rm -rf ./.sysupdatelog
-echo  上次更新是在 `date +"%Y-%m-%d %H:%M:%S"` >> ./.sysupdatelog
-#删除并重新创建文件
-read -p " 键入选项:  1.pacman  2.aur  3.flatpak  4.清理  (默认1-3)" num
-if [[ $num -eq 1 ]];  
-    then  
-    echo 开始全面系统更新
-    sudo pacman -Syu --noconfirm
-elif [[ $num -eq 2 ]];  
-    then
-    echo 开始aur仓库更新  
-    yay -Syu --noconfirm
-elif [[ $num -eq 3 ]];  
-    then
-    echo 开始flatpak更新  
-    flatpak update
-elif [[ $num -eq 4 ]];  
-    then
-    echo 开始清理
-    sudo pacman -Scc
-    yay -scc
-    flatpak uninstall --unused
-else  
-    echo 开始所有更新
-    sudo pacman -Syu --noconfirm
-    flatpak update
-    yay -Syu --noconfirm
-fi
-echo 完事
+echo "上次更新是在 $(date +"%Y-%m-%d %H:%M:%S")" > ./.sysupdatelog
+
+# 删除并重新创建文件
+echo "键入选项:  1.pacman  2.aur  3.flatpak  4.清理  (默认1-3)"
+read ass
+
+# 根据用户输入执行相应操作
+case "$ass" in
+    1)
+        echo "开始全面系统更新"
+        sudo pacman -Syu --noconfirm
+        ;;
+    2)
+        echo "开始aur仓库更新"
+        yay -Syu --noconfirm
+        ;;
+    3)
+        echo "开始flatpak更新"
+        flatpak update
+        ;;
+    4)
+        echo "开始清理"
+        sudo pacman -Scc
+        yay -scc
+        flatpak uninstall --unused
+        ;;
+    *)
+        echo "开始所有更新"
+        sudo pacman -Syu --noconfirm
+        flatpak update 
+        yay -Syu --noconfirm
+        ;;
+esac
+
+# 结束脚本
+echo "完事"
 exit
