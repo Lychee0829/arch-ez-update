a3b3cfbe155f58a041d969836a68fcc1b85f6152
Lychee0829 <qweasdzxc1230829@163.com>
2024-12-26 18:35:22 +0800

    Add files via upload

--- update.sh
+++ update.sh
@@ -2,32 +2,26 @@
 
 # 显示系统信息
 neofetch
-
-# 读取并显示 .sysupdatelog 文件内容
-cat .sysupdatelog | while read line; do
-    echo "$line"
-done
-
+# 先帝创业未半而中道崩殂
+trap 'echo "退出了嗷" && kill -1 $$' SIGINT
 # 读取上次更新时间
-rm -rf ./.sysupdatelog
-echo "上次更新是在 $(date +"%Y-%m-%d %H:%M:%S")" > ./.sysupdatelog
+grep -e -Syu /var/log/pacman.log | tail -1
 
-# 删除并重新创建文件
 echo "键入选项:  1.pacman  2.aur  3.flatpak  4.清理  (默认1-3)"
-read ass
+read choice
 
-# 根据用户输入执行相应操作
-case "$ass" in
+# 执行相应操作
+case "$choice" in
     1)
-        echo "开始全面系统更新"
+        echo "开始全面更新系统"
         sudo pacman -Syu --noconfirm
         ;;
     2)
-        echo "开始aur仓库更新"
+        echo "开始更新Aur软件包"
         yay -Syu --noconfirm
         ;;
     3)
-        echo "开始flatpak更新"
+        echo "开始更新flatpak软件包"
         flatpak update
         ;;
     4)
@@ -44,6 +38,6 @@ case "$ass" in
         ;;
 esac
 
-# 结束脚本
+# 结束
 echo "完事"
 exit
